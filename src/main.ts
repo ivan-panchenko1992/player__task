f11c04a2dca51254ee6f58f57be9b9f08e1c0780
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-04-21 19:46:29 +0300

    maybeThisHAlf

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,70 @@
 const container  = document.querySelector('#video-box')
 const vastUrl = 'https://cdn.admixer.net/public/player%2Fregular-preroll.xml';
 
-function fetching(url: string) {
-  const result = fetch(url).then(respose => respose.text()).then(result => result);
-  return result;
+let a:object = {};
+
+async function fetching (url: string) {
+  let resultObj: any;
+  resultObj = await fetch(url)
+  .then(response => response.text())
+  .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+  .then(result => result.all).then(result => Object.values(result))
+  .then(result => result.filter(element => (element.attributes.type)))
+  .then(result=> result.filter(el => el.attributes.type.textContent=== 'video/mp4'))
+  .then(result => result[0].innerHTML)
+  .then(result => container.insertAdjacentHTML('afterbegin',`<video src="${result}"></video>`))
+  return resultObj;
 }
- 
-const toFetch = fetching(vastUrl);
+
+
+// async function http<T>(
+//   request: RequestInfo
+// ): Promise<T> {
+//   const response = await fetch(request);
+//   const body = await response.text();
+//   return body;
+// }
+ let b:any;
+ let toFetch =   fetching(vastUrl)
+//  fetch(vastUrl).then(response => response.text()).then(result => {
+//    console.log(typeof result) 
+//    a = result});
 
 console.log(toFetch)
 
 
 class Player {
   url: string;
-
-  
+  prepearedXML: any;
+ 
   setVastUrl(vastUrl: string) {
     this.url = vastUrl;
+  }
 
+  load(vastUrl: string) {
+    this.prepearedXML = fetch(vastUrl)
+    .then(response => response.text())
+    .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+    .then(result => result.all).then(result => Object.values(result))
+    .then(result => result.filter(element => (element.attributes.type)))
+    .then(result=> result.filter(el => el.attributes.type.textContent=== 'video/mp4'))
+  }
+
+  async start() {
+    let resultString: any;
+    await this.prepearedXML.then((result: { innerHTML: any; }[]) => resultString =  result[0].innerHTML)
+    container.insertAdjacentHTML('afterbegin',`<video src="${resultString} width="400" height="300"></video>`)
+  }
+
+  play() {
+
+  }
+
+  pause() {
+
+  }
+
+  close() {
+
+  }
 }
-
-
